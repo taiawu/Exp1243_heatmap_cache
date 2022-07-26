b972e1e821405061198ebe16105af553ec96c96f
taiawu <taiawu@gmail.com>
2022-07-25 17:43:12 -0700

    add heatmap_ggproto object

---
+++ R/heatmap_ggproto.R
@@ -0,0 +1,9 @@
+#' A ggproto object of the heatmap plot
+#'
+#' Ready to display
+#'
+#' @format get to this later
+#   ...
+# }
+
+"heatmap_ggproto"
---
+++ data-raw/heatmap_ggproto.R
@@ -0,0 +1,3 @@
+## code to prepare `heatmap_ggproto` dataset goes here
+heatmap_ggproto <- readRDS("~/Box Sync/data/Exp1351--20220725_dye_screen_explorer_functions/1_intermediate/Exp1350--20220725_heatmap_ggproto.rds")
+usethis::use_data(heatmap_ggproto, overwrite = TRUE)
